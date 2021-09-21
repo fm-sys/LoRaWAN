b4b9aa22bbbe788b764650837b9e3de08aa10ca2
fm-sys <64581222+fm-sys@users.noreply.github.com>
2021-09-21 22:45:21 +0200

    add chart library to WIP android app

--- LoRaLogViewer/app/src/main/java/com/fmsys/logviewer/MainActivity.java
+++ LoRaLogViewer/app/src/main/java/com/fmsys/logviewer/MainActivity.java
@@ -1,14 +1,187 @@
 package com.fmsys.logviewer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IFillFormatter;
+import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    LineChart chart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        chart = findViewById(R.id.chart);
+
+        // background color
+        chart.setBackgroundColor(Color.WHITE);
+
+        // disable description text
+        chart.getDescription().setEnabled(false);
+
+        // enable touch gestures
+        chart.setTouchEnabled(true);
+
+        chart.setDrawGridBackground(true);
+
+//        // create marker to display box when values are selected
+//        MyMarkerView mv = new MyMarkerView(this, R.layout.custom_marker_view);
+//
+//        // Set the marker to the chart
+//        mv.setChartView(chart);
+//        chart.setMarker(mv);
+
+        // enable scaling and dragging
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(true);
+
+
+        XAxis xAxis = chart.getXAxis();
+
+            // vertical grid lines
+            //xAxis.enableGridDashedLine(10f, 10f, 0f);
+
+
+        YAxis yAxis = chart.getAxisLeft();
+
+            // disable dual axis (only use LEFT axis)
+            chart.getAxisRight().setEnabled(false);
+
+            // horizontal grid lines
+            //yAxis.enableGridDashedLine(10f, 10f, 0f);
+
+            // axis range
+            //yAxis.setAxisMaximum(200f);
+            //yAxis.setAxisMinimum(-50f);
+
+
+        // // Create Limit Lines // //
+
+        LimitLine ll1 = new LimitLine(150f, "Upper Limit");
+        ll1.setLineWidth(4f);
+        ll1.enableDashedLine(10f, 10f, 0f);
+        ll1.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_TOP);
+
+        LimitLine ll2 = new LimitLine(-30f, "Lower Limit");
+        ll2.setLineWidth(4f);
+        ll2.enableDashedLine(10f, 10f, 0f);
+        ll2.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
+
+        // draw limit lines behind data instead of on top
+        yAxis.setDrawLimitLinesBehindData(true);
+        xAxis.setDrawLimitLinesBehindData(true);
+
+        // add limit lines
+        yAxis.addLimitLine(ll1);
+        yAxis.addLimitLine(ll2);
+        //xAxis.addLimitLine(llXAxis);
+
+
+
+
+        // add data
+        setData();
+
+        // draw points over time
+        chart.animateXY(1000, 1000);
+
+        // get the legend (only possible after setting data)
+        Legend l = chart.getLegend();
+
+        // draw legend entries as lines
+        l.setForm(Legend.LegendForm.LINE);
+
+    }
+
+    private void setData() {
+
+        ArrayList<Entry> values = new ArrayList<>();
+
+        for (int x = 0; x < 45; x++) {
+
+            float y = (float) (Math.random() * (float) 180) - 30;
+            values.add(new Entry(x, y));
+        }
+
+        LineDataSet set1;
+
+        if (chart.getData() != null &&
+                chart.getData().getDataSetCount() > 0) {
+            set1 = (LineDataSet) chart.getData().getDataSetByIndex(0);
+            set1.setValues(values);
+            set1.notifyDataSetChanged();
+            chart.getData().notifyDataChanged();
+            chart.notifyDataSetChanged();
+        } else {
+            // create a dataset and give it a type
+            set1 = new LineDataSet(values, "DataSet 1");
+
+            set1.setDrawIcons(false);
+
+            // draw dashed line
+            set1.enableDashedLine(10f, 5f, 0f);
+
+            // black lines and points
+            set1.setColor(Color.BLACK);
+            set1.setCircleColor(Color.BLACK);
+
+            // line thickness and point size
+            set1.setLineWidth(1f);
+            set1.setCircleRadius(3f);
+
+            // draw points as solid circles
+            set1.setDrawCircleHole(false);
+
+            // customize legend entry
+            set1.setFormLineWidth(1f);
+            set1.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
+            set1.setFormSize(15.f);
+
+            // text size of values
+            set1.setValueTextSize(9f);
+
+            // draw selection line as dashed
+            set1.enableDashedHighlightLine(10f, 5f, 0f);
+
+            // set the filled area
+            //set1.setDrawFilled(true);
+            //set1.setFillFormatter((dataSet, dataProvider) -> chart.getAxisLeft().getAxisMinimum());
+
+            // set color of filled area
+
+                //Drawable drawable = ContextCompat.getDrawable(this, R.drawable.fade_red);
+                //set1.setFillDrawable(drawable);
+
+
+            ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+            dataSets.add(set1); // add the data sets
+
+            // create a data object with the data sets
+            LineData data = new LineData(dataSets);
+
+            // set data
+            chart.setData(data);
+        }
     }
 }
