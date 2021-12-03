ce1532ad13e2cddc01f2c96c6a54712a4aa546e9
fm-sys <64581222+fm-sys@users.noreply.github.com>
2021-12-03 21:03:31 +0100

    code formatting + airtime messurement

--- test_p2p_sender/test_p2p_sender.ino
+++ test_p2p_sender/test_p2p_sender.ino
@@ -1,6 +1,6 @@
 /*
-Copied from https://github.com/Apress/begin-lora-radio-networks-with-arduino/blob/master/Chapter%204/sensor.ino, which corresponds to the code published 
-in the book "Beginning LoRa Radio Networks with Arduino: Build Long Range, Low Power Wireless IoT Networks by Pradeeka Seneviratne (Apress, 2019)".
+  Copied from https://github.com/Apress/begin-lora-radio-networks-with-arduino/blob/master/Chapter%204/sensor.ino, which corresponds to the code published
+  in the book "Beginning LoRa Radio Networks with Arduino: Build Long Range, Low Power Wireless IoT Networks" by Pradeeka Seneviratne (Apress, 2019).
 
 */
 #include <RH_RF95.h> //RadioHead RFM9x library
@@ -10,13 +10,13 @@ in the book "Beginning LoRa Radio Networks with Arduino: Build Long Range, Low P
 #define RFM95_RST 9
 #define RFM95_INT 2
 
+//Frequency
 #define RF95_FREQ 868.0
 
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
 
-void setup()
-{
+void setup() {
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
 
@@ -24,13 +24,14 @@ void setup()
   Serial.begin(9600);
   delay(100);
 
+  // reset LoRa Shield
   digitalWrite(RFM95_RST, LOW);
   delay(10);
   digitalWrite(RFM95_RST, HIGH);
   delay(10);
 
   while (!rf95.init()) {
-    Serial.println("initializing…");
+    Serial.println("Initializing failed");
     while (1);
   }
   Serial.println("initialisation succeeded");
@@ -42,50 +43,45 @@ void setup()
   Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
 
   rf95.setTxPower(23, false);
-
 }
 
-void loop()
-{
-  
-  char radiopacket[20]= "hello world";
-  
-  //dtostrf(t,5,2,radiopacket);
+void loop() {
+  char radiopacket[20] = "hello world";
 
-  Serial.print("Send '"); Serial.print(radiopacket); Serial.println("'");
-  
+  Serial.print("Send '");
+  Serial.print(radiopacket);
+  Serial.println("'");
+  Serial.println("Sending...");
+  delay(10);
 
-  Serial.println("Sending..."); delay(10);
- 
+  long t1 = millis();
   rf95.send((uint8_t *) radiopacket, 20);
 
-  Serial.println("Waiting for packet to complete..."); delay(10);
+  Serial.println("Waiting to complete...");
+  delay(10);
   rf95.waitPacketSent();
-  
-// Waiting for the reply
+  Serial.print(millis() - t1);
+  Serial.println("ms Übertragungsdauer");
+
+
+  // Waiting for the reply
   uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
   uint8_t len = sizeof(buf);
 
-  Serial.println("Waiting for reply..."); delay(10);
-  if (rf95.waitAvailableTimeout(1000))
-  {
-    // Should be a reply message for us now
-    if (rf95.recv(buf, &len))
-    {
+  Serial.println("Waiting for reply...");
+  delay(10);
+  if (rf95.waitAvailableTimeout(1000)) {
+    if (rf95.recv(buf, &len)) {
       Serial.print("Got reply: '");
       Serial.print((char*)buf);
       Serial.println("'");
       Serial.print("RSSI: ");
       Serial.println(rf95.lastRssi(), DEC);
-    }
-    else
-    {
+    } else {
       Serial.println("Receive failed");
     }
-  }
-  else
-  {
-    Serial.println("No reply, is the receiver running?");
+  } else {
+    Serial.println("No reply");
   }
   delay(30 * 1000);
 }
